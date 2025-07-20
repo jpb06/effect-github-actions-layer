ca85ae6e2147f034095be8f3ba4acfdf3bb8fe6d
jpb06 <jpb.06@outlook.com>
2025-07-20 16:25:00 +0200

    fix: export githubactionsops interface

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './errors/github-actions-layer.error.js';
+export type { GithubActionsOps } from './layer/github-actions.context.js';
 export * from './layer/github-actions.layer.js';
 export * from './layer/github-actions.live.js';
 export { GithubContext } from './types/github-context.type.js';
